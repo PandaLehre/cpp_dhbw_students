9b38100b07f672da1e49ee6ca89bf876b126247f
Christian <chris-bader@hotmail.de>
2025-06-25 13:11:13 +0200

    Revert "added game starter code"
    This reverts commit b5f4a8b66f812c08c84dec0f2eb90e3e05f978fa.

--- 08_exam/02_game_starter_code/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-#include <string>
-
-int main() {
-    std::string cascadeFilePath = "haarcascade_frontalface_default.xml";
-    Game game(cascadeFilePath);
-    game.run();
-    return 0;
-}
